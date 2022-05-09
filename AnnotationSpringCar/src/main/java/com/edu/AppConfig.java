0c280efa3e257feb5d2392612ceaba4b02a968a6
Thara <Thara@Thara-PC>
2022-05-09 19:25:49 +0530

    Annotation Third commit

---
+++ AnnotationSpringCar/src/main/java/com/edu/AppConfig.java
@@ -0,0 +1,15 @@
+package com.edu;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig 
+{
+	@Bean
+	public Car getCar()
+	{
+		return new Car();
+	}
+
+}
---
+++ AnnotationSpringCar/src/main/java/com/edu/Car.java
@@ -0,0 +1,9 @@
+package com.edu;
+
+public class Car 
+{
+public void carFunction()
+{
+	System.out.println("Super High speed Car");
+}
+}
